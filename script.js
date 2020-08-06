a67a21592e41fcb0a605061ce7c429d57ba977b0
tawfiq3 <twfq26@gmail.com>
2020-08-06 21:28:29 +0600

    inputed pin are now showing on display

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function getRandomNumber(min,max){
     let step1 = max-min+1;
     let step2 = Math.random() * step1;
@@ -7,12 +6,32 @@ function getRandomNumber(min,max){
     return result;
 }
 
-
 const gPin = document.getElementById("pin-button")
 gPin.addEventListener("click",function(){  
     let randomNumber = getRandomNumber(1000,9999); 
     document.getElementById("pin-output").value = randomNumber
     console.log("pin-output");
 })
+
+function getNumber(btnId){
+    const btn = document.getElementById(btnId);
+    const dPinNumber = parseInt(btn.innerText);
+    console.log(dPinNumber)
+
+    const numShown = document.getElementById("dialedPin").value;
+    console.log(numShown)
+    document.getElementById("dialedPin").value = numShown + dPinNumber;
+    
+}
+
+// //** Number Handler */
+// function numberClickHandler(Btn) {
+//     const btn = document.getElementById(Btn)
+//     const btnNumber = parseInt(btn.innerText)
+
+//     const inputBox = document.getElementById("inputBox").value
+//     document.getElementById("inputBox").value = inputBox + btnNumber;
+
+
 
 
