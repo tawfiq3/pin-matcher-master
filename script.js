bb9adb2fd25f86c047cbcdb4f9d3e79f83cdd195
tawfiq3 <twfq26@gmail.com>
2020-08-07 12:09:31 +0600

    Assignment Done

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Hiding Notification Part
+
+document.getElementById("noMatch").style.display='none'
+document.getElementById("match").style.display='none'
+
+//Random Number Generating Function
+
 function getRandomNumber(min,max){
     let step1 = max-min+1;
     let step2 = Math.random() * step1;
@@ -6,32 +13,81 @@ function getRandomNumber(min,max){
     return result;
 }
 
+// Showing Generated Random Function On display
+
 const gPin = document.getElementById("pin-button")
 gPin.addEventListener("click",function(){  
     let randomNumber = getRandomNumber(1000,9999); 
     document.getElementById("pin-output").value = randomNumber
-    console.log("pin-output");
 })
+
+//Getting Number from number section to display
 
 function getNumber(btnId){
     const btn = document.getElementById(btnId);
     const dPinNumber = parseInt(btn.innerText);
-    console.log(dPinNumber)
 
     const numShown = document.getElementById("dialedPin").value;
-    console.log(numShown)
-    document.getElementById("dialedPin").value = numShown + dPinNumber;
-    
+    document.getElementById("dialedPin").value = numShown + dPinNumber;    
 }
 
-// //** Number Handler */
-// function numberClickHandler(Btn) {
-//     const btn = document.getElementById(Btn)
-//     const btnNumber = parseInt(btn.innerText)
+//Backspace Function
 
-//     const inputBox = document.getElementById("inputBox").value
-//     document.getElementById("inputBox").value = inputBox + btnNumber;
+function backSpace(){
+    const display = document.getElementById("dialedPin")
+    const lastDigit = display.value;
+    display.value = lastDigit.substring(0,lastDigit.length - 1);   
+}
 
+//All clear Function
+
+function allClear(){
+    document.getElementById("dialedPin").value = ""
+}
+
+//Function For Submit Button
+
+function submitBtn(){
+    const randomNumber= document.getElementById("pin-output").value;
+    const dialedPin = document.getElementById("dialedPin").value;
+
+    if( randomNumber =='' || dialedPin == ''){
+        alert('Your Input Field is empty')
+    }
+    else{
+        if(randomNumber == dialedPin){
+            document.getElementById("match").style.display = 'block'
+            document.getElementById("pin-button").textContent = "Pin Matched" 
+            document.getElementById("pin-output").value = ""
+            document.getElementById("dialedPin").value = ""
+        }
+        else{
+            document.getElementById("noMatch").style.display = 'block'
+            document.getElementById("pin-output").style.display = "Incorrect Pin"
+            document.getElementById("pin-output").value = ""
+            document.getElementById("dialedPin").value = ""
+            trialleft()
+        }
+     }
+}
+
+//Function for trial left
+
+let btnPressed = 0;
+function trialleft(){
+    const tryLeft = document.getElementById("tryLeft")
+    btnPressed++
+    if(btnPressed == 1){
+        tryLeft.innerText = "2 more try left"
+    }
+    if(btnPressed == 2){
+        tryLeft.innerText = "1 try left"
+    }
+    if(btnPressed == 3){
+        tryLeft.innerText = "Sorry! You can't try anymore"
+    }
+
+}
 
 
 
